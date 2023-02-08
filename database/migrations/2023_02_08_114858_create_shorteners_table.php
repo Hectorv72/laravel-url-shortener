922c4c5ed1ba2f117b18506ef0ba00adabab33ba
Hectorv72 <hectorvaldezfsa13@gmail.com>
2023-02-08 11:24:43 -0300

    first commit

---
+++ database/migrations/2023_02_08_114858_create_shorteners_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shorteners', function (Blueprint $table) {
+            $table->id();
+            $table->string('linked_url', 5);
+            $table->mediumText('shortened_url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shorteners');
+    }
+};
