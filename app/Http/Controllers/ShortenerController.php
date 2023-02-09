ae5e36a76503f53cf93f7768cb424a71bd25816c
Hectorv72 <hectorvaldezfsa13@gmail.com>
2023-02-09 11:37:03 -0300

    agrega funcionalidad de crear shorteners

--- app/Http/Controllers/ShortenerController.php
+++ app/Http/Controllers/ShortenerController.php
@@ -5,11 +5,52 @@ namespace App\Http\Controllers;
 use App\Models\Shortener;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Str;
+use Exception;
+use PDOException;
 
 class ShortenerController extends Controller
 {
+    private $errorMessage = ["message" => "Ocurrió un error, vuelva a intentarlo más tárde"];
+
     public function all()
     {
         return new JsonResponse(Shortener::all());
+    }
+
+    public function find($id = null)
+    {
+        return new JsonResponse(Shortener::find($id));
+    }
+
+    /**
+     * Busca entre los registros la url enviada y verifica su existencia
+     * En caso de encontrarla: Devuelve como respuesta la información del registro existente
+     * En caso de no existir: Crea un nuevo registro con la información
+     * @return JsonResponse
+     */
+    public function create(Request $request)
+    {
+        $user = null;
+        try {
+            $shortener = Shortener::where([['linked_url', $request->input()], ['user_id', $user]])->first();
+
+            if (!$shortener) {
+                $shortener = new Shortener([
+                    'shortened_url' => Str::random(5),
+                    'linked_url' => $request->input('url'),
+                    'user_id' => $user
+                ]);
+                $shortener->save();
+            }
+            return new JsonResponse($shortener->setHidden(['updated_at', 'created_at', 'user_id', 'id'])->toArray());
+        } catch (PDOException $e) {
+            if ($e->getCode() == 23000) {
+                return new JsonResponse(["message" => "Los campos enviados no corresponden a un objeto, revise y vuelva a intentarlo"], 400);
+            }
+            return new JsonResponse($this->errorMessage, 500);
+        } catch (Exception $e) {
+            return new JsonResponse($this->errorMessage, 500);
+        }
     }
 }
--- app/Models/Shortener.php
+++ app/Models/Shortener.php
@@ -9,10 +9,17 @@ class Shortener extends Model
 {
     use HasFactory;
 
+    protected $primaryKey = 'id';
+
     protected $fillable = [
         'user_id',
         'shortened_url',
         'linked_url',
         'interactions'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
