4978b2a4d5668a5685bfffeb93c907a6270b7fec
Hectorv72 <hectorvaldezfsa13@gmail.com>
2023-02-10 10:20:27 -0300

    arregla errores en ShortenerController

--- app/Http/Controllers/ShortenerController.php
+++ app/Http/Controllers/ShortenerController.php
@@ -12,26 +12,32 @@ use PDOException;
 class ShortenerController extends Controller
 {
 
-    private function serverErrorMessage()
+    private function serverErrorMessage($e)
     {
+        error_log($e->getMessage());
         return new JsonResponse(["message" => "Ocurrió un error, vuelva a intentarlo más tárde"], 500);
     }
 
     public function all()
     {
         try {
-            return new JsonResponse(Shortener::all());
+            return new JsonResponse(Shortener::all(), 200);
         } catch (Exception $e) {
-            return $this->serverErrorMessage();
+            return $this->serverErrorMessage($e);
         }
     }
 
     public function find($key = null)
     {
         try {
-            return new JsonResponse(Shortener::where(['shortener_key', $key])->first());
+            $shortener = Shortener::where('shortened_key', $key)->first();
+            if ($shortener) {
+                return new JsonResponse($shortener->setHidden(['updated_at', 'created_at', 'user_id', 'id']), 200);
+            } else {
+                return new JsonResponse(["message" => "No existe un acortador con esa clave"], 404);
+            }
         } catch (Exception $e) {
-            return $this->serverErrorMessage();
+            return $this->serverErrorMessage($e);
         }
     }
 
@@ -49,20 +55,20 @@ class ShortenerController extends Controller
 
             if (!$shortener) {
                 $shortener = new Shortener([
-                    'shortened_url' => Str::random(5),
+                    'shortened_key' => Str::random(5),
                     'linked_url' => $request->input('url'),
                     'user_id' => $user
                 ]);
                 $shortener->save();
             }
-            return new JsonResponse($shortener->setHidden(['updated_at', 'created_at', 'user_id', 'id'])->toArray());
+            return new JsonResponse($shortener->setHidden(['updated_at', 'created_at', 'user_id', 'id'])->toArray(), 200);
         } catch (PDOException $e) {
             if ($e->getCode() == 23000) {
                 return new JsonResponse(["message" => "Los campos enviados no corresponden a un objeto, revise y vuelva a intentarlo"], 400);
             }
-            return $this->serverErrorMessage();
+            return $this->serverErrorMessage($e);
         } catch (Exception $e) {
-            return $this->serverErrorMessage();
+            return $this->serverErrorMessage($e);
         }
     }
 }
