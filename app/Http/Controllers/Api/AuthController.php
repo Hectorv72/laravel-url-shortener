e4124ee6382c12f5126ba499351ac7ef5a5f2b06
Hectorv72 <hectorvaldezfsa13@gmail.com>
2023-02-13 10:01:44 -0300

    agrega manejo de errores a AuthController

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,16 +4,22 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
+use Exception;
 
 class AuthController extends Controller
 {
+    private function serverErrorMessage(Exception $e)
+    {
+        error_log($e->getMessage());
+        return response()->json(['message' => 'Lo sentimos, ocurrió un error vuelva a intentarlo más tárde'], Response::HTTP_INTERNAL_SERVER_ERROR);
+    }
+
     public function register(Request $request)
     {
         try {
@@ -35,38 +41,51 @@ class AuthController extends Controller
             return response()->json($user, Response::HTTP_CREATED);
         } catch (ValidationException $e) {
             return response()->json($e->errors(), Response::HTTP_UNPROCESSABLE_ENTITY);
+        } catch (Exception $e) {
+            return $this->serverErrorMessage($e);
         }
     }
 
     public function login(Request $request)
     {
-        $credentials = $request->validate([
-            'email' => ['required', 'email'],
-            'password' => ['required'],
-        ]);
+        try {
+            $credentials = $request->validate([
+                'email' => ['required', 'email'],
+                'password' => ['required'],
+            ]);
 
-        if (Auth::attempt($credentials)) {
-            $user = Auth::user();
-            $token = $user->createToken('token')->plainTextToken;
-            $cookie = cookie('cookie_token', $token, 60 * 24);
-            return response(["token" => $token], Response::HTTP_OK)->withCookie($cookie);
-        } else {
-            return response(["message" => "Credenciales invalidas"], Response::HTTP_UNAUTHORIZED);
+            if (Auth::attempt($credentials)) {
+                $token = Auth::user()->createToken('token')->plainTextToken;
+                $cookie = cookie('cookie_token', $token, 60 * 24);
+                return response(["token" => $token], Response::HTTP_OK)->withCookie($cookie);
+            } else {
+                return response(["message" => "Credenciales invalidas"], Response::HTTP_UNAUTHORIZED);
+            }
+        } catch (Exception $e) {
+            return $this->serverErrorMessage($e);
         }
     }
 
-    public function profile(Request $request)
+    public function profile()
     {
-        return response()->json([
-            "message" => "user profile ok",
-            "user" => auth()->user(),
-        ], Response::HTTP_OK);
+        try {
+            return response()->json([
+                "message" => "user profile ok",
+                "user" => auth()->user(),
+            ], Response::HTTP_OK);
+        } catch (Exception $e) {
+            return $this->serverErrorMessage($e);
+        }
     }
 
     public function logout()
     {
-        $cookie = Cookie::forget('cookie_token');
-        return response(["message" => "Cierre de session ok"], Response::HTTP_ACCEPTED)->withCookie($cookie);
+        try {
+            $cookie = Cookie::forget('cookie_token');
+            return response(["message" => "Cierre de session ok"], Response::HTTP_ACCEPTED)->withCookie($cookie);
+        } catch (Exception $e) {
+            return $this->serverErrorMessage($e);
+        }
     }
 
     public function all()
@@ -74,8 +93,7 @@ class AuthController extends Controller
         try {
             return response()->json(["users" => User::all()]);
         } catch (Exception $e) {
-            error_log($e->getMessage());
-            return response()->json(["message" => "ocurrio un error vuelve a intentarlo mas tarde"]);
+            return $this->serverErrorMessage($e);
         }
     }
 }
