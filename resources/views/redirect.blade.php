44c659fe523c9abbbdfa3532ed13f262894578b0
Hectorv72 <hectorvaldezfsa13@gmail.com>
2023-02-21 15:53:39 -0300

    agrega el template base para las views

--- resources/views/redirect.blade.php
+++ resources/views/redirect.blade.php
@@ -1,30 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('base')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <title>Shorty</title>
+@section('title')
+    Shorty - redirect
+@endsection
+
+@section('head')
     <style>
         .image {
-            width: 90%;
+            width: 45%;
+            height: auto;
         }
     </style>
-</head>
+@endsection
 
-<body>
-    <div class="position-absolute top-50 start-50 translate-middle">
-        <img src="{{ asset('images/shorty_splash.png') }}" class=" image" />
+@section('body')
+    <div>
+        <img src="{{ asset('images/shorty_splash.png') }}" class="image position-absolute top-50 start-50 translate-middle" />
     </div>
+@endsection
 
+@section('script')
     <script>
         setTimeout(() => {
             document.location.replace("{{ $urldir }}")
         }, 1000);
     </script>
-</body>
-
-</html>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('base')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <title>Laravel</title>
+@section('head')
     <style>
         code {
             color: #444;
@@ -16,9 +10,9 @@
             padding: 10px;
         }
     </style>
-</head>
+@endsection
 
-<body>
+@section('body')
     <h4>Peticiones HTTP - API REST Shorty</h4>
     <div class="row gap-4 m-2 mt-4">
         <div>
@@ -60,6 +54,4 @@
             </code>
         </div>
     </div>
-</body>
-
-</html>
+@endsection
