c39823e53bfcf54f9656ef4d039e72623e2a6267
Hectorv72 <hectorvaldezfsa13@gmail.com>
2023-02-21 15:17:11 -0300

    agrega descripción a las peticiones de la api

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,29 +4,62 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Laravel</title>
     <style>
-        pre code {
+        code {
+            color: #444;
             background-color: #eee;
             border: 1px solid #999;
             display: block;
-            padding: 3px;
+            padding: 10px;
         }
     </style>
 </head>
 
 <body>
-    <h4>Peticiones</h4>
-    <pre>
-        <code>
-            {{ route('api.shortener.find', ['key' => 'shortener_key!']) }}
-        </code>
-    </pre>
-    <pre>
-        <code>
-            {{ route('api.shortener.all') }}
-        </code>
-    </pre>
+    <h4>Peticiones HTTP - API REST Shorty</h4>
+    <div class="row gap-4 m-2 mt-4">
+        <div>
+            <p>Obtiene la información de todos los acortadores:</p>
+            <div>
+                <code>
+                    <span class="badge bg-success me-2">GET: </span>
+                    <a href="{{ route('api.shortener.all') }}" target="blank">
+                        {{ route('api.shortener.all') }}
+                    </a>
+                </code>
+            </div>
+        </div>
+
+
+        <div>
+            <p>Crea un acortador:</p>
+            <code>
+                <span class="badge bg-danger me-2">POST:</span>
+                <span>{{ route('api.shortener.create') }}</span>
+            </code>
+        </div>
+
+        <div>
+            <p>Obtiene información a cerca de un acortador específico:</p>
+            <code>
+                <span class="badge bg-success me-2">GET: </span>
+                <a href="{{ route('api.shortener.find', ['key' => 'shortener-key']) }}" target="blank">
+                    {{ route('api.shortener.find', ['key' => 'shortener-key']) }}
+                </a>
+            </code>
+        </div>
+
+        <div>
+            <p>Crea un acortador:</p>
+            <code>
+                <span class="badge bg-danger me-2">POST:</span>
+                <span>{{ route('api.auth.login') }}</span>
+            </code>
+        </div>
+    </div>
 </body>
 
 </html>
